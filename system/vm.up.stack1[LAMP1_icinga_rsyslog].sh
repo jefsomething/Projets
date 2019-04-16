6b03ccf772f56fad829a3eca80234a31890b7dad
jef.something <jef.something@gmail.com>
2019-04-16 10:23:16 +0200

    deleted:    vm.halt.sh 	deleted:    vm.up.sh 	deleted:    vm.up.stack1[LAMP1_icinga_rsyslog].sh

--- system/vm.up.stack1[LAMP1_icinga_rsyslog].sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#which VMs to start:
-N1=LAMP1_
-N2=icinga_
-N3=rsyslog_
-
-#start VMs one at a time easier on memory resource on)
-for i in $N1 $N2 $N3; do VBoxManage startvm "$i" --type headless; sleep 5; done
