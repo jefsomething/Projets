2cf5a886d665a1b2edcf2e1749d8f7ea63f8ac5c
jef.something <jef.something@gmail.com>
2019-05-20 15:50:52 +0200

    python basics

--- java/testsUnitaires/02_tests sur chaines et permutations/src/coa_ex3/TestTimeManager.java
+++ java/testsUnitaires/02_tests sur chaines et permutations/src/coa_ex3/TestTimeManager.java
@@ -17,38 +17,38 @@ public class TestTimeManager {
 	
 	@Test
 	public void testNull_shouldReturnNOT_POSSIBLE() {
-		assertEquals(NOT_POSSIBLE, manager.getFirstTimeAvailable(null));
+		assertEquals(NOT_POSSIBLE, TimeManager.getFirstTimeAvailable(null));
 	}
 
 	@Test
 	public void testEmpty_shouldReturnNOT_POSSIBLE() {
-		assertEquals(NOT_POSSIBLE, manager.getFirstTimeAvailable(""));
+		assertEquals(NOT_POSSIBLE, TimeManager.getFirstTimeAvailable(""));
 	}
 
 	@Test
 	public void test123456_shouldReturn12h34m56() {
-		assertEquals("12h34m56s", manager.getFirstTimeAvailable("123456"));
+		assertEquals("12h34m56s", TimeManager.getFirstTimeAvailable("123456"));
 	}
 
 	@Test
 	public void test132845_shouldReturn12h34m58() {
-		assertEquals("12h34m58s", manager.getFirstTimeAvailable("132845"));
+		assertEquals("12h34m58s", TimeManager.getFirstTimeAvailable("132845"));
 	}
 
 	@Test
 	public void test438129_shouldReturn12h38m49s() {
 //		assertEquals("12h48m39s", manager.getFirstTimeAvailable("438129"));
-		assertEquals("12h38m49s", manager.getFirstTimeAvailable("438129"));
+		assertEquals("12h38m49s", TimeManager.getFirstTimeAvailable("438129"));
 	}
 
 	@Test
 	public void test709324_shouldReturn02h37m49s() {
 //		assertEquals("23h47m09s", manager.getFirstTimeAvailable("709324"));
-		assertEquals("02h37m49s", manager.getFirstTimeAvailable("709324"));
+		assertEquals("02h37m49s", TimeManager.getFirstTimeAvailable("709324"));
 	}
 
 	@Test
 	public void test183679_shouldReturnNOT_POSSIBLE() {
-		assertEquals(NOT_POSSIBLE, manager.getFirstTimeAvailable("183679"));
+		assertEquals(NOT_POSSIBLE, TimeManager.getFirstTimeAvailable("183679"));
 	}
 }
---
+++ python/basics/main.py
@@ -0,0 +1,28 @@
+from foo import *
+
+matchGame()
+'''
+inputValidation()
+
+print(divAbyB(30,6))
+print(divAbyB(30,0))
+print(divAbyB(30,5))
+
+n = 5
+print('!',n,' = ',(fact(n)))
+
+res = Fibonacci(n)
+print('Fibonnaci ',n,',',res)
+
+print('fibonacci sequence ...')
+fibonacci(3,5)
+
+print('delta array ...')
+for i in range(1):
+    growNShrink(25)
+
+displayDelta(10)
+
+print(gaussian(101))
+print(randNb)
+'''
